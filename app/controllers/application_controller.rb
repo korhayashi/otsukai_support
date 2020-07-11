70c8f723eef237417bde00a2598b2df4dd7b4657
kaori hayashi <suzu_33@hotmail.com>
2020-07-12 05:47:47 +0900

    deviseでのログイン機能、新規会員登録機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:category])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:postal_code])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:address])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:phone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:admin])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:category])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:family_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:postal_code])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:address])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:phone_number])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:admin])
+  end
 end
---
+++ db/migrate/20200711204223_change_data_postal_code_to_user.rb
@@ -0,0 +1,5 @@
+class ChangeDataPostalCodeToUser < ActiveRecord::Migration[5.2]
+  def change
+    change_column :users, :postal_code, :string
+  end
+end
